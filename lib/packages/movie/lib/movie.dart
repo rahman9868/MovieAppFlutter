b70e3803ee316b399d5a73753524e9a8ee2d0114
chairul <rahman9868@gmail.com>
2024-01-16 10:57:04 +0700

    Modularisation (unversion file)  - core  - movie  - tv_show  - about

---
+++ lib/packages/movie/lib/movie.dart
@@ -0,0 +1,17 @@
+library movie;
+
+export 'presentation/bloc/detail/movie_detail_bloc.dart';
+export 'presentation/bloc/recommendations/movies_recommendations_list_bloc.dart';
+export 'presentation/bloc/list/now_playing_movies_bloc.dart';
+export 'presentation/bloc/list/popular_movies_bloc.dart';
+export 'presentation/bloc/list/top_rated_movies_bloc.dart';
+export 'presentation/bloc/search/search_bloc.dart';
+export 'presentation/bloc/watchlist/watchlist_movies_bloc.dart';
+export 'presentation/bloc/watchlist_status/watchlist_movie_status_cubit.dart';
+export 'presentation/pages/home_movie_page.dart';
+export 'presentation/pages/movie_detail_page.dart';
+export 'presentation/pages/popular_movies_page.dart';
+export 'presentation/pages/top_rated_movies_page.dart';
+export 'presentation/pages/search_page.dart';
+export 'presentation/pages/watchlist_movies_page.dart';
+export 'presentation/widgets/movie_card_list.dart';
---
+++ lib/packages/tv_show/lib/tv_show.dart
@@ -0,0 +1,18 @@
+library tv_show;
+
+export 'presentation/bloc/detail/tv_show_detail_bloc.dart';
+export 'presentation/bloc/recommendations/tv_show_recommendations_list_bloc.dart';
+export 'presentation/bloc/list/now_playing_tv_shows_bloc.dart';
+export 'presentation/bloc/list/popular_tv_shows_bloc.dart';
+export 'presentation/bloc/list/top_rated_tv_shows_bloc.dart';
+export 'presentation/bloc/search/search_tv_show_bloc.dart';
+export 'presentation/bloc/watchlist/watchlist_tv_shows_bloc.dart';
+export 'presentation/bloc/watchlist_status/watchlist_tv_show_status_cubit.dart';
+export 'presentation/pages/home_tv_show_page.dart';
+export 'presentation/pages/tv_show_detail_page.dart';
+export 'presentation/pages/popular_tv_shows_page.dart';
+export 'presentation/pages/now_playing_tv_shows_page.dart';
+export 'presentation/pages/top_rated_tv_shows_page.dart';
+export 'presentation/pages/watchlist_tv_shows_page.dart';
+export 'presentation/pages/search_page_tv_show.dart';
+export 'presentation/widgets/tv_show_card_list.dart';
